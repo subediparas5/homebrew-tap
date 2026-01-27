abb16b83e88dffabc8dd944940b7edfd883b4a9a
Parash Subedi <parash.subedi@grepsr.com>
2026-01-27 12:28:03 +0545

    Refactor Lsport formula to use GitHub URL and tag for version 0.1.1, remove platform-specific URLs, and update installation method to use Cargo.

--- Formula/lsport.rb
+++ Formula/lsport.rb
@@ -4,31 +4,15 @@
 class Lsport < Formula
   desc "A TUI for managing local and remote ports via SSH"
   homepage "https://github.com/subediparas5/lsport"
-  version "0.1.1"
+  url "https://github.com/subediparas5/lsport.git",
+      tag: "v0.1.1"
   license "MIT"
+  head "https://github.com/subediparas5/lsport.git", branch: "main"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/subediparas5/lsport/releases/download/v0.1.1/lsport-linux-x86_64.tar.gz"
-      sha256 "e638e7180ea4b362cdc0c871cd5481c2f29a1ee71f1837a6efe363adc1ec1d98"
-    else
-      url "https://github.com/subediparas5/lsport/releases/download/v#{version}/lsport-macos-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_MACOS_X86_64"
-    end
-  end
-
-  on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/subediparas5/lsport/releases/download/v#{version}/lsport-linux-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64"
-    else
-      url "https://github.com/subediparas5/lsport/releases/download/v#{version}/lsport-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_X86_64"
-    end
-  end
+  depends_on "rust" => :build
 
   def install
-    bin.install "lsport"
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
