910701f2014ea3a1c2b8a8e384e756d23ed33426
Parash Subedi <subediparas5@gmail.com>
2026-01-27 12:15:09 +0545

    lsport 0.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/lsport.rb
+++ Formula/lsport.rb
@@ -4,13 +4,13 @@
 class Lsport < Formula
   desc "A TUI for managing local and remote ports via SSH"
   homepage "https://github.com/subediparas5/lsport"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/subediparas5/lsport/releases/download/v#{version}/lsport-macos-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_MACOS_ARM64"
+      url "https://github.com/subediparas5/lsport/releases/download/v0.1.1/lsport-linux-x86_64.tar.gz"
+      sha256 "e638e7180ea4b362cdc0c871cd5481c2f29a1ee71f1837a6efe363adc1ec1d98"
     else
       url "https://github.com/subediparas5/lsport/releases/download/v#{version}/lsport-macos-x86_64.tar.gz"
       sha256 "PLACEHOLDER_MACOS_X86_64"
